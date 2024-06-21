a0458f2297dd2960fc1f8d321b73b0a606d834f9
Amir Hesham Ibrahim <amirhesham65@gmail.com>
2024-06-21 16:04:45 +0300

    modify: REPL include parser

--- repl/repl.go
+++ repl/repl.go
@@ -7,7 +7,7 @@ import (
 	"os"
 
 	"github.com/amirhesham65/hera-lang/lexer"
-	"github.com/amirhesham65/hera-lang/token"
+	"github.com/amirhesham65/hera-lang/parser"
 )
 
 func Start(userName string, in io.Reader, out io.Writer) {
@@ -26,10 +26,22 @@ func Start(userName string, in io.Reader, out io.Writer) {
 		}
 
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		// Output source code tokens
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
